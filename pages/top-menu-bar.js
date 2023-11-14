b9d253ef2312305ba6b7b6bf7bab43062a9c390c
Tzur Paldi <tzur.paldi@outlook.com>
2023-11-14 13:26:23 +0200

    updated search selector

--- pages/top-menu-bar.js
+++ pages/top-menu-bar.js
@@ -1,7 +1,7 @@
 import {SearchBarPanel} from './search-bar-panel';
 export class TopMenuBar {
     get searchButton() {
-        return web.findElement(".header__secondary-nav [href='/search']");
+        return web.findElement(".header__secondary-nav a:first-of-type");
     }
     clickSearch(text) {
         this.searchButton.click();
