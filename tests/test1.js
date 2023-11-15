5f01e5da571d68916d91f56d2b9b1cd62e4255b5
Tzur Paldi <tzur.paldi@outlook.com>
2023-11-15 14:27:31 +0200

    added transactions

--- tests/test1.js
+++ tests/test1.js
@@ -2,15 +2,15 @@ import * as testBase from "./test-base";
 
 var mainPage = testBase.webApp.getMainPage();
 
+web.transaction("*** closing popup");
 mainPage.popup.close();
 
 var searchBarPanelPage = mainPage.topMenuBar.clickSearch();
-
+web.transaction("*** Searching text");
 searchBarPanelPage.searchText ("Honzo");
-
+web.transaction("***Select first result");
 var productPage = searchBarPanelPage.selectResult('0');
 
 var title = productPage.getTitle();
 log.info(`Title = ${title}`);
 assert.contain(title, "title");
-web.pause(10000);
